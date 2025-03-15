f3f67a4067fbc0a708165d6be7acab44052b76c8
Nicolas Orgias <norgias@gmail.com>
2025-03-15 10:45:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
+import TalentHub from './pages/TalentHub';
 import Footer from './components/Footer';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/contact" element={<ContactPage />} />
+          <Route path="/talent-hub" element={<TalentHub />} />
         </Routes>
         <Footer />
       </div>
